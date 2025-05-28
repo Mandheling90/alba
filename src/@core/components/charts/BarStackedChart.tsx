82832385ad1dbdc91a5747f5b76d3215a30d9f9b
LeeSeungYong <du58ck@gmail.com>
2025-05-29 00:14:27 +0900

    히트맵 제외한 스크롤 생성 처리

--- src/@core/components/charts/BarStackedChart.tsx
+++ src/@core/components/charts/BarStackedChart.tsx
@@ -1,34 +1,51 @@
-import Highcharts from 'highcharts'
+import type { Options } from 'highcharts'
+import Highcharts from 'highcharts/highstock'
 import React, { useEffect } from 'react'
 import { ICountBarChart } from 'src/model/statistics/StatisticsModel'
 import styled from 'styled-components'
 
 const BarStackedChart: React.FC<{ data: ICountBarChart }> = ({ data }) => {
   useEffect(() => {
-    // 하이차트 차트 생성 (중첩된 형태)
-    Highcharts.chart({
+    const isScrollEnabled = data.xcategories.length > Number(process.env.NEXT_PUBLIC_CHART_SCROLL_COUNT)
+
+    const commonOptions: Options = {
       chart: {
         renderTo: 'container',
-        type: 'column'
+        type: 'column',
+        panning: {
+          enabled: false,
+          type: 'x' as const
+        }
       },
       title: {
         text: '24시간 입장자 및 퇴장자 비교'
       },
       xAxis: {
         categories: data.xcategories,
+        type: 'category',
+        crosshair: false,
         title: {
           text: data.xtitle
+        },
+        labels: {
+          style: {
+            fontSize: '12px'
+          },
+          formatter: function (this: Highcharts.AxisLabelsFormatterContextObject) {
+            return data.xcategories[this.pos]
+          }
         }
       },
       yAxis: {
         min: 0,
+        crosshair: false,
         title: {
           text: data.ytitle
         }
       },
       tooltip: {
-        shared: true,
-        headerFormat: '<b>{point.key}</b><br/>'
+        shared: false,
+        headerFormat: ''
       },
       plotOptions: {
         column: {
@@ -36,10 +53,15 @@ const BarStackedChart: React.FC<{ data: ICountBarChart }> = ({ data }) => {
           borderWidth: 0,
           dataLabels: {
             enabled: true,
-            formatter: function () {
+            formatter: function (this: Highcharts.Point) {
               return this.y === 0 ? '' : this.y
             }
           }
+        },
+        series: {
+          enableMouseTracking: true,
+          stickyTracking: false,
+          animation: false
         }
       },
       legend: {
@@ -63,7 +85,45 @@ const BarStackedChart: React.FC<{ data: ICountBarChart }> = ({ data }) => {
           color: '#2CAFFE'
         }
       ]
-    })
+    }
+
+    const stockOptions: Options = {
+      ...commonOptions,
+      navigator: {
+        enabled: isScrollEnabled,
+        xAxis: {
+          labels: {
+            enabled: false
+          }
+        },
+        series: [
+          {
+            type: 'column',
+            name: data.chartDataList[0].name,
+            data: data.chartDataList[0].dataList,
+            color: '#544FC5'
+          },
+          {
+            type: 'column',
+            name: data.chartDataList[1].name,
+            data: data.chartDataList[1].dataList,
+            color: '#2CAFFE'
+          }
+        ]
+      },
+      rangeSelector: {
+        enabled: isScrollEnabled
+      },
+      scrollbar: {
+        enabled: isScrollEnabled
+      }
+    }
+
+    if (isScrollEnabled) {
+      Highcharts.stockChart(stockOptions)
+    } else {
+      Highcharts.chart(commonOptions)
+    }
   }, [data])
 
   return (
@@ -75,6 +135,9 @@ const BarStackedChart: React.FC<{ data: ICountBarChart }> = ({ data }) => {
 
 const ChartWrapper = styled.div`
   .highcharts-credits {
+    display: none;
+  }
+  .highcharts-range-selector-group {
     display: none;
   }
 `
