6efef82faf1a361d175048a0795ba1987342b482
LeeSeungYong <du58ck@gmail.com>
2025-06-03 10:03:31 +0900

    아이콘 수직 정렬

--- src/@core/components/molecule/PipelineTitle.tsx
+++ src/@core/components/molecule/PipelineTitle.tsx
@@ -11,10 +11,16 @@ const PipelineTitle: FC<IPipelineTitle> = ({ Icon, title, marginBottom = 0 }): R
   return (
     <Box display='flex' alignItems='center' mb={marginBottom}>
       {(title ?? []).map((t, i) => (
-        <Box key={t} sx={{ display: 'flex', alignItems: 'center' }}>
-          {i === 0 && Icon && <Box mr={2}>{Icon}</Box>}
-          <Typography variant={i === 0 ? 'h6' : 'body1'}>{t}</Typography>
-          {i !== (title?.length ?? 0) - 1 && <Typography sx={{ mx: 3 }}>|</Typography>}
+        <Box key={t} sx={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+          {i === 0 && Icon && (
+            <Box mr={2} sx={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+              {Icon}
+            </Box>
+          )}
+          <Typography variant={i === 0 ? 'h6' : 'body1'} sx={{ lineHeight: 1 }}>
+            {t}
+          </Typography>
+          {i !== (title?.length ?? 0) - 1 && <Typography sx={{ mx: 3, lineHeight: 1 }}>|</Typography>}
         </Box>
       ))}
     </Box>
